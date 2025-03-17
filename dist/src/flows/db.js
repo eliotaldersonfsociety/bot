9651b70d074815a58783bfe16ce4a7fd5fcfac8b
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-17 05:05:24 -0500

    Create db.js

---
+++ dist/src/flows/db.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql2/promise';
+const pool = mysql.createPool({
+    host: 'host.docker.internal',
+    user: 'root',
+    password: '',
+    database: 'whatsapp_bot_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+async function insertarReserva(data) {
+    const { reservationDate, peopleCount, fullName, contactNumber, comments } = data;
+    const query = 'INSERT INTO reservas (fecha_hora, num_personas, nombre, contacto, comentarios) VALUES (?, ?, ?, ?, ?)';
+    const values = [reservationDate, peopleCount, fullName, contactNumber, comments];
+    try {
+        const result = await pool.query(query, values);
+        console.log('Reserva insertada correctamente:', result);
+        return result;
+    }
+    catch (error) {
+        console.error('Error al insertar reserva:', error);
+        throw error;
+    }
+}
+export default insertarReserva;
